eb716a3dac6657d1052d77650e4f458e1b0bb0e4
Will Hawkins <hawkinsw@obs.cr>
2025-07-28 08:28:39 -0400

    Add a Second Plugin -- Source
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

---
+++ src/source.c
@@ -0,0 +1,31 @@
+#include "plugin.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+int load() {
+  printf("Loaded source plugin!\n");
+  return 1;
+}
+
+char *plugin_name = "source";
+
+char *name() { return plugin_name; }
+
+generate_result_t generate(ip_addr_t source, ip_addr_t target, body_p body) {
+
+  printf("I was given a source ip address: -%s-\n", stringify_ip(source));
+  printf("I was given a target ip address: -%s-\n", stringify_ip(target));
+
+  generate_result_t result;
+
+  result.destination.type = INET_ADDR_V4;
+  result.destination.addr.ipv4.s_addr = inet_addr("8.8.8.8");
+  result.body.data = (uint8_t *)malloc(50);
+  result.body.len = 50;
+
+  return result;
+}
