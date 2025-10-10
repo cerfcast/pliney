e23024142f046391f8c228d67e3fbc9f4abf876c
Will Hawkins <hawkinsw@obs.cr>
2025-10-10 10:06:45 -0400

    CI/CD: Use RUNNER_TEMP (really)
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- test/log_tests.sh
+++ test/log_tests.sh
@@ -2,7 +2,7 @@
 
 ./build/pliney -type dgram  \!\> body ./test/data/dns_cnn =\> target 8.8.8.8 53  =\> log  -mode overwrite ${RUNNER_TEMP}/testing.pcap
 
-dd bs=1 skip=68 if=${RUNNER_TEMP}testing.pcap > ${RUNNER_TEMP}/testing.pcap.slim 
+dd bs=1 skip=68 if=${RUNNER_TEMP}/testing.pcap > ${RUNNER_TEMP}/testing.pcap.slim 
 diff test/data/dns_cnn ${RUNNER_TEMP}/testing.pcap.slim
 
 result=$?
