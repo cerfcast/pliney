e208c3b351bb0cf3f1521b24370926a90051a286
Will Hawkins <hawkinsw@obs.cr>
2026-01-03 01:59:04 -0500

    Forgot To Commit Directory
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

---
+++ unit_tests/pliney_transport.cpp
@@ -0,0 +1,28 @@
+#include "lib/safety.hpp"
+#include <gtest/gtest.h>
+#include <packetline/constants.hpp>
+#include <pisa/pisa.h>
+
+TEST(PlineyTransport, from_native_transport) {
+  EXPECT_EQ(Pliney::Transport::ICMP, Pliney::from_native_transport(PLINEY_ICMP));
+  EXPECT_EQ(Pliney::Transport::ICMP6, Pliney::from_native_transport(PLINEY_ICMP6));
+  EXPECT_EQ(Pliney::Transport::UDP, Pliney::from_native_transport(PLINEY_UDP));
+  EXPECT_EQ(Pliney::Transport::TCP, Pliney::from_native_transport(PLINEY_TCP));
+}
+
+TEST(PlineyTransport, to_native_transport) {
+  EXPECT_EQ(PLINEY_ICMP, Pliney::to_native_transport(Pliney::Transport::ICMP));
+  EXPECT_EQ(PLINEY_ICMP6, Pliney::to_native_transport(Pliney::Transport::ICMP6));
+  EXPECT_EQ(PLINEY_TCP, Pliney::to_native_transport(Pliney::Transport::TCP));
+  EXPECT_EQ(PLINEY_UDP, Pliney::to_native_transport(Pliney::Transport::UDP));
+}
+
+TEST(PlineyTransport, to_native_transport_unreachable) {
+  EXPECT_THROW(Pliney::from_native_transport(0), PlineyUnreachable);
+}
+
+
+
+
+
+
